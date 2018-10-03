58dd16301be5e01dff6a309a0868984329c99537
kayartaya.vinod <kayartaya.vinod@gmail.com>
2018-10-03 11:52:21 +0530

    🇮🇳

---
+++ ex01.py
@@ -0,0 +1,16 @@
+# This is my first python script file
+# A python script file is also known as a 'module'
+# The name of the file is the name of the module
+
+print('Hello, Python!')
+
+message = '''My name is Vinod.
+I am from Bangalore.
+Today I am at Philips, Manyata.'''
+
+print(message);
+
+my_name, my_city, my_age = 'Vinod', 'Bangalore', 45
+txt = my_name + ' is from ' + my_city + ' and is ' + str(my_age) + ' years old.'
+print(txt)
+
---
+++ ex02.py
@@ -0,0 +1,12 @@
+"""This program will ask the user to enter a number
+and print if it is less / more than 100
+"""
+
+num = input('Enter a number: ') # returns a str
+num = int(num) # create a new 'int' instance by supplying 'str' object
+
+if num < 100:
+    print('%d is less than %d' % (num, 100))
+else:
+    print('{} is more than or equals to {}'.format(num, 100));
+
