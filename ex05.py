02fad0a3132e9d942c5936b99c833cb480b0c622
kayartaya.vinod <kayartaya.vinod@gmail.com>
2018-10-03 14:53:35 +0530

    🇮🇳

--- ex05.py
+++ ex05.py
@@ -2,7 +2,8 @@ def main():
     while True:
         m = int(input('Enter month: '))
         y = int(input('Enter year: '))
-        # as a good practice, import must be done in the beginning of the module
+        # as a good practice, import must be done in the 
+        # beginning of the module
         from myutils import max_days # loads first time; and gets from cache subsequently
         md = max_days(m, y)
         print('{}/{} has a maximum of {} days'.format(m, y, md))
