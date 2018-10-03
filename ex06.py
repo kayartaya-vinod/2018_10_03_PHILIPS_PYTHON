3d51e988aaab95e0f1b79ee795377ceeb7da8a88
kayartaya.vinod <kayartaya.vinod@gmail.com>
2018-10-03 15:27:09 +0530

    🇮🇳

---
+++ ex06.py
@@ -0,0 +1,16 @@
+#! /usr/local/bin/python3
+
+def print_table_1():
+    num = int(input('Enter a number: '))
+    for n in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+        print('{} X {} = {}'.format(num, n, num*n))
+
+def print_table(num, limit=10):
+    for n in range(1, limit + 1):
+        print('{} X {} = {}'.format(num, n, num*n))
+
+def main():
+    num = int(input('Enter a number: '))
+    print_table(num)
+
+if __name__=='__main__': main()
